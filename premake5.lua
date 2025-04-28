aa3082de74b028cf4df22bbef40edbfc7bf8de48
Jacob Swindell <jacob.swindell@btinternet.com>
2025-04-28 09:22:44 +0100

    add macos support

--- premake5.lua
+++ premake5.lua
@@ -4,6 +4,7 @@ configurations { "Debug", "Release" }
 project "VoroPathplanner"
 kind "ConsoleApp"
 language "C++"
+cppdialect "C++17"
 targetdir "bin/%{cfg.buildcfg}"
 
 files { "./src/**.cpp", "./src/**.hpp" }
@@ -13,7 +14,12 @@ filter "system:linux"
 links { "m", "raylib", "lua" }
 
 filter "system:macosx"
+architecture "arm64"
+defines { "CY_NO_INTRIN_H" }
+includedirs { "/opt/homebrew/include/lua", "/usr/local/include" }
+libdirs { "/opt/homebrew/lib/lua", "/usr/local/lib" }
 links { "m", "raylib", "lua", "Cocoa.framework", "IOKit.framework", "OpenGL.framework" }
+buildoptions { "-isystem/opt/homebrew/include/lua" }
 
 filter "configurations:Debug"
 defines { "DEBUG" }
